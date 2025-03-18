2bcbf14e5682aa30dc42cdb55c64b6b7f0fdfa5b
ljh0401 <78478247+ljh0401@users.noreply.github.com>
2025-03-18 16:16:26 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 123 ms, Memory: 28,800 KB -BaekjoonHub

---
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.LinkedList;
+
+class Solution
+{
+	static Scanner sc = new Scanner(System.in);
+	static List<Integer> list;
+	public static void main(String args[]) throws Exception
+	{
+		
+		int T = 10;
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int N = sc.nextInt();
+			list = new LinkedList<>();
+			for (int i = 0; i < N; i++) {
+				list.add(sc.nextInt());
+			}
+			int M = sc.nextInt();
+			for (int i = 0; i < M; i++) {
+				String word = sc.next();
+				if (word.equals("I")) {
+					I();
+				}
+			}
+			System.out.print("#"+test_case);
+			for (int i = 0; i < 10; i++) {
+				System.out.print(" "+list.get(i));
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void I() {
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		for (int i = 0; i < y; i++) {
+			list.add(x+i, sc.nextInt());
+		}
+	}
+	
+}
